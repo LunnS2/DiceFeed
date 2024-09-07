00698d212b92128dddda00f6fe5751b8bec8c5bd
Lunn <thafm.work@gmail.com>
2024-09-07 22:52:02 +0100

    Update Profile and Post components; Update page.jsx from both api/profile and api/profile/[id]

---
+++ components/Profile.jsx
@@ -0,0 +1,25 @@
+import UserPost from "./Post";
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className='w-full'>
+      <h1 className='text-left'>
+        <span>{name} Profile</span>
+      </h1>
+      <p className='mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl text-left'>{desc}</p>
+
+      <div className='mt-10'>
+        {data.map((post) => (
+          <UserPost
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
