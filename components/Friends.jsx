e0693e65c15156ca99d9d82643fec0f0929b0ac1
Lunn <thafm.work@gmail.com>
2024-09-14 13:37:42 +0100

    Add detailed comments to Friends component for functionality clarification

---
+++ components/Friends.jsx
@@ -0,0 +1,20 @@
+/* Search Mechanism */
+// Users will have an "Add Friends" button in the SideNavbar that takes them to a page where they can search for other users by name.
+// The app should display the matching user, along with an "Add" button next to their name.
+
+/* Add Friends Functionality */
+// Users should be able to search for others by typing their name and send a friend request.
+// If they are already friends, a message should notify the user that they are already connected.
+// If they are not friends, the user can send a friend request.
+// If a request has already been sent, a message should inform the user that the request is pending.
+// If a request is pending, then a new request shouldn't be sent.
+// The user should also be able to cancel a pending friend request.
+
+  
+/* Accept/Decline Friend Requests */
+// When the user receives a friend request, they should have the option to either accept or decline it.
+// The friend request should remain visible until the user accepts, declines, or the other party cancels the request.
+
+
+
+
