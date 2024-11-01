ebfbb13893ccf997497de09884e8f2aaa96a5097
Lunn <thafm.work@gmail.com>
2024-11-01 00:00:32 +0000

    Add user image to profile

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,14 +1,25 @@
 // my-next-app\components\Profile.jsx
 
+import { useSession } from "next-auth/react";
 import UserPost from "./Post";
+import Image from "next/image";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  
   return (
-    <section className='w-full'>
+    <section className='flex w-full text-center'>
       <h1 className='text-left'>
         <span>{name} Profile</span>
       </h1>
       <p className='mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl text-left'>{desc}</p>
+      <Image
+            src={session?.user?.image || "/default-profile.png"}
+            alt={`${session?.user?.username || "User"}'s profile`}
+            width={40}
+            height={40}
+            className="rounded-full object-cover"
+          />
 
       <div className='mt-10'>
         {data.map((post) => (
